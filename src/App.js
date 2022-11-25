8b919775e681e74beb070831024f230688701534
shirbalas <shir29291@gmail.com>
2022-11-25 11:44:42 +0200

    Add shoppingList in App.js and shoppingItem component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import ShoppingItem from "./components/ShoppingItem";
+import "./App.css";
 
 function App() {
+  const shoppingList = [
+    { productName: "soap kit", price: 30, productPhoto: "product1.jpeg" },
+    { productName: "hands soap", price: 20, productPhoto: "product2.jpeg" },
+    { productName: "comb", price: 10, productPhoto: "product3.jpeg" },
+    { productName: "shampoo", price: 50, productPhoto: "product4.jpeg" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <h1>shopping List:</h1>
+      </div>
+      {shoppingList.map((item, index) => {
+        return (
+          <ShoppingItem
+            key={index}
+            productName={item.productName}
+            price={item.price}
+            productPhoto={item.productPhoto}
+          ></ShoppingItem>
+        );
+      })}
     </div>
   );
 }
